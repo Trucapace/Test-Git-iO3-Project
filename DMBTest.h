2ebca43cdee743c9e9fa3d9dafc9e2ce10bd11de
David Blanck <davidblanck@3n396r1.us.terumo.com>
2014-03-07 13:07:07 -0500

    added a test class

---
+++ DMBTest.h
@@ -0,0 +1,13 @@
+//
+//  DMBTest.h
+//  Git Test
+//
+//  Created by David Blanck on 3/7/14.
+//  Copyright (c) 2014 VidWare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMBTest : NSObject
+
+@end
---
+++ DMBTest.m
@@ -0,0 +1,13 @@
+//
+//  DMBTest.m
+//  Git Test
+//
+//  Created by David Blanck on 3/7/14.
+//  Copyright (c) 2014 VidWare. All rights reserved.
+//
+
+#import "DMBTest.h"
+
+@implementation DMBTest
+
+@end
